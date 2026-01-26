27e6e299cbffd3676180b1e92e99efb4924c6ee6
RLucas1102 <robenoltl@gmail.com>
2026-01-26 11:14:12 -0500

    Defined create_attr for each valid type; maybe? I don't know really

--- base/DynamicalState.C
+++ base/DynamicalState.C
@@ -23,9 +23,24 @@ namespace pba {
 
     DynamicalStateData::DynamicalStateData(const DynamicalStateData &d) {}
 
-    DynamicalStateData &DynamicalStateData::operator=(const DynamicalStateData &d)
-    {
-        // TODO: insert return statement here
+    void DynamicalStateData::create_attr(const std::string &nam, const int &def) {
+        DSAttribute<int> newAttrib = DSAttribute<int>(nam, def);
+        int_attributes.insert({nam, newAttrib});
+    }
+
+    void DynamicalStateData::create_attr(const std::string &nam, const float &def) {
+        DSAttribute<float> newAttrib = DSAttribute<float>(nam, def);
+        float_attributes.insert({nam, newAttrib});
+    }
+
+    void DynamicalStateData::create_attr(const std::string &nam, const Vector &def) {
+        DSAttribute<Vector> newAttrib = DSAttribute<Vector>(nam, def);
+        vector_attributes.insert({nam, newAttrib});
+    }
+
+    void DynamicalStateData::create_attr(const std::string &nam, const Color &def) {
+        DSAttribute<Color> newAttrib = DSAttribute<Color>(nam, def);
+        color_attributes.insert({nam, newAttrib});
     }
 
     DynamicalState CreateDynamicalState(const std::string& nam) {
