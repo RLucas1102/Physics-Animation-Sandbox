f9b781cbdec75cfbc75ddd68a3689d4698568ad2
RLucas1102 <robenoltl@gmail.com>
2026-01-25 23:09:29 -0500

    Added DynamicalState and AABB; renamed all C files to cpp to work with intellisense; some problem that I did not have a time for a better solution for

---
+++ base/DynamicalState.cpp
@@ -0,0 +1,15 @@
+//-------------------------------------------------------
+ //
+ //  DynamicalState.h
+ //
+ //  Container for data associated with the dynamics
+ //  degrees of freedom in a system.
+ //
+ //  Copyright (c) 2017 Jerry Tessendorf
+ //
+ //
+ //--------------------------------------------------------
+
+#include "DynamicalState.h"
+
+using namespace pba;
