e386d66db9625a42d5c773c1f2fada65cf18d433
RLucas1102 <robenoltl@gmail.com>
2026-01-26 00:27:53 -0500

    Started defining constructor and create function

--- base/DynamicalState.C
+++ base/DynamicalState.C
@@ -11,5 +11,27 @@
  //--------------------------------------------------------
 
 #include "DynamicalState.h"
+#include <iostream>
 
-using namespace pba;
+namespace pba {
+
+    DynamicalStateData::DynamicalStateData(const std::string& nam) :
+        name                (nam),
+        t                   (0.0),
+        nb_items            (0)
+        {}
+
+    DynamicalStateData::DynamicalStateData(const DynamicalStateData &d) {}
+
+    DynamicalStateData &DynamicalStateData::operator=(const DynamicalStateData &d)
+    {
+        // TODO: insert return statement here
+    }
+
+    DynamicalState CreateDynamicalState(const std::string& nam) {
+        DynamicalState outState = DynamicalState(new DynamicalStateData(nam));
+        std::cout << "Created DynamicalState: " << nam << std::endl;
+        return outState;
+    }
+
+}
