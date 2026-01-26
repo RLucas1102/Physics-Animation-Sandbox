a0feb6b0ae2de3110ddc33ed8cc102191aa1f268
RLucas1102 <robenoltl@gmail.com>
2026-01-26 13:42:58 -0500

    Added ParticleState class, declared/defined some member functions, and vector of particles

--- base/ParticleSystem.C
+++ base/ParticleSystem.C
@@ -1,10 +1,41 @@
 #include "ParticleSystem.h"
 
+using namespace pba;
+
 ParticleSystem::ParticleSystem(const std::string &nam) :
     name (nam)
     {}
 
-PSYS CreateParticleSystem(const std::string& nam) {
+void ParticleSystem::AddParticle() {
+    particles.push_back(ParticleState());
+}
+
+void ParticleSystem::AddParticles(const size_t p) {
+    for (size_t i = 0; i < p; i++) {
+        particles.push_back(ParticleState());
+    }
+    
+}
+
+ParticleState ParticleSystem::GetParticle(const size_t p) {
+    return particles[p];
+}
+
+size_t const ParticleSystem::Psize() {
+    return particles.size();
+} 
+
+void ParticleSystem::Pclear() {
+    particles.clear();
+}
+
+void const ParticleSystem::ShowParticles() {
+    for (particles_it = particles.begin(); particles_it != particles.end(); particles_it++) {
+        std::cout << particles_it->mass << std::endl;
+    }
+}
+
+PSYS pba::CreateParticleSystem(const std::string& nam) {
     PSYS outSys = PSYS( new ParticleSystem(nam) );
     std::cout << "ParticleSystem Created: " << outSys->Name() << std::endl;    
     return outSys;
--- include/ParticleSystem.h
+++ include/ParticleSystem.h
@@ -6,21 +6,69 @@
 #include <string>
 #include <memory>
 #include <iostream>
+#include <vector>
 
-class ParticleSystem {
-    public:
-        ParticleSystem (const std::string& nam = "ParticleSysNoName");
+namespace pba {
 
-        ~ParticleSystem() {};
+    ////////////////////////////////////////////////
+    //
+    //      PARTICLE STATE
+    //
+    // The state of a particle is characterized by
+    // (1) particle positions
+    // (2) particle velocities
+    // (3) particle masses
+    // (4) particle colors - useful for display
+    ////////////////////////////////////////////////
+    class ParticleState {
+        public:
+            ParticleState() :
+                position(Vector(0,0,0)),
+                velocity(Vector(0,0,0)),
+                color(Color(1,1,1,1)),
+                mass(1.0)
+                {};
+        
+            ~ParticleState(){};	   
 
-        const std::string& Name() const {return name;}
+            Vector position;
+            Vector velocity;
+            Color color;
+            float mass;
+    };
 
-    private:
-        const std::string name;
-};
+    class ParticleSystem {
+        public:
+            ParticleSystem (const std::string& nam = "ParticleSysNoName");
 
-typedef std::shared_ptr<ParticleSystem> PSYS;
+            ~ParticleSystem() {};
 
-PSYS CreateParticleSystem(const std::string& nam = "ParticleSysNoName");
+            const std::string& Name() const {return name;}
+
+            void AddParticle();
+
+            void AddParticles(const size_t p);
+
+            ParticleState GetParticle(const size_t p);
+
+            size_t const Psize();
+
+            void Pclear();
+
+            void const ShowParticles();
+
+        private:
+            const std::string name;
+
+            // This is all of the particles in the system
+            std::vector<ParticleState> particles;
+            std::vector<ParticleState>::iterator particles_it;
+    };
+
+    typedef std::shared_ptr<ParticleSystem> PSYS;
+
+    PSYS CreateParticleSystem(const std::string& nam = "ParticleSysNoName");
+
+}
 
 #endif
