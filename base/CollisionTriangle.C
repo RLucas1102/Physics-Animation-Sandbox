073c85eabe0cd55b29d1a91e24182b11659cb8db
RLucas1102 <robenoltl@gmail.com>
2026-01-28 22:46:34 -0500

    Starting collisions with triangles; implemented hit function, constructor, and create function for a single triangle

---
+++ base/CollisionTriangle.C
@@ -0,0 +1,49 @@
+#include "Vector.h"
+#include "CollisionTriangle.h"
+
+using namespace pba;
+
+CollisionTriangleRaw::CollisionTriangleRaw(const Vector& p0, const Vector& p1, const Vector& p2) : 
+    P0 (p0),
+    P1 (p1),
+    P2 (p2)
+    {
+        e1 = p1 - p0;
+        e2 = p2 - p0;
+
+        normal = e1 ^ e2;
+
+    }
+
+bool CollisionTriangleRaw::hit(const Vector& XS, const Vector& VS, const double dt, Vector& XH, double& dtH) {
+    bool hit = false;
+    
+    Vector XU = XS + VS * dt;
+
+    Vector Normal = normal / normal.magnitude();
+
+    double fXS = normal * (XS - P0);
+    double fXU = normal * (XU - P0);
+    if (fXU == 0 || fXU * fXS < 0) {
+        Vector XH_candidate = XS + VS * ((Normal * (P0 - XS) / (Normal * VS)));
+        double dtH_candidate = (Normal * (P0 - XS) / (Normal * VS));
+
+        double u = normal * ((XH_candidate - P0) ^ e2) / (normal * normal);
+        double v = normal * ((e1 ^ (XH_candidate - P0))) / (normal * normal);
+        if ( (u >= 0 && u <= 1) &&
+             (v >= 0 && v <= 1) &&
+             (u + v >= 0 && u + v <= 1) ) {
+
+            XH = XH_candidate;
+            dtH = dtH_candidate;
+            hit = true;
+
+        }
+    }
+
+    return hit;
+}
+
+CollisionTriangle pba::MakeCollisionTriangle(const Vector& p0, const Vector& p1, const Vector& p2) {
+    return CollisionTriangle(new CollisionTriangleRaw(p0, p1, p2));
+}
---
+++ include/CollisionTriangle.h
@@ -0,0 +1,34 @@
+#ifndef COLLISION_TRIANGLE_H
+#define COLLISION_TRIANGLE_H
+
+#include "Vector.h"
+#include "memory"
+
+namespace pba {
+    class CollisionTriangleRaw {
+        public:
+
+            CollisionTriangleRaw(const Vector& p0, const Vector& p1, const Vector& p2);
+            ~CollisionTriangleRaw(){};
+
+            bool hit( const Vector& XS, const Vector& VS, const double dt, Vector& XH, double& dtH);
+
+        private:
+
+            Vector P0;
+            Vector P1;
+            Vector P2;
+            Vector e1;
+            Vector e2;
+            Vector normal;
+
+    };
+
+    typedef std::shared_ptr<CollisionTriangleRaw> CollisionTriangle;
+
+    CollisionTriangle MakeCollisionTriangle(const Vector& p0, const Vector& p1, const Vector& p2);
+
+}
+
+
+#endif
