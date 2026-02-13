1b2baf07db32897bb65855b839e3c8b01095514d
RLucas1102 <robenoltl@gmail.com>
2026-02-13 14:24:25 -0500

    Changed a comment

--- things/MyThing.h
+++ things/MyThing.h
@@ -77,7 +77,7 @@ class MyThing: public PbaThingyDingy
     // flag for whether to create more particles
     bool emit;
    
-    // Class member variables to simulate gravity and collisions
+    // Class member variables
     PSYS MyThing_PSYS;
     GISolver solver;
     Force accumulator;
