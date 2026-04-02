79914e3c376312002d377e3cd3cf66fb74805b43
RLucas1102 <robenoltl@gmail.com>
2026-04-02 15:54:52 -0400

    Implemented MOI calculation and added some setup to fill angular rotation, velocity, and momentum

--- base/RigidBodySystem.C
+++ base/RigidBodySystem.C
@@ -31,6 +31,7 @@ void RigidBodySystem::ComputeRBDData() {
         SetLeverArm(p, d);
     }
     
+    // Setup linear velocity
     Vector initVel = Vector(0,0,0);
     for (size_t p = 0; p < Psize(); p++)
     {
@@ -41,6 +42,15 @@ void RigidBodySystem::ComputeRBDData() {
 
     _linearVel = initVel;
 
+    // Setup rotation matrix
+    _angularRot = unitMatrix();
+
+    // Setup angular velocity
+    _angularVel = pba::Vector(drand48() * 2 - 1, drand48() * 2 - 1, drand48() * 2 - 1);
+
+    // Setup angular momentum
+    RecomputeMOI();
+    _angularMom = _momentOfInertia * _angularVel;
     
 }
 
@@ -49,12 +59,32 @@ void RigidBodySystem::ComputeM() {
 }
 
 void RigidBodySystem::RecomputeMOI() {
+    Matrix delta = unitMatrix();
 
+    for (size_t p = 0; p < Psize(); p++)
+    {
+        double mass = GetMass(p);
+        Vector pos  = GetLeverArm(p);
+
+        for (size_t i = 0; i < 3; i++)
+        {
+            for (size_t j = 0; j < 3; j++)
+            {
+                double value = _momentOfInertia(i,j) + (mass * (delta(i,j) * pos.magnitude() - pos[i] * pos[j]));
+                _momentOfInertia.Set(i, j, value);
+            }
+            
+        }
+        
+    }
+
+    _inverseMomentOfInertia = inverse(_momentOfInertia);
+    
 }
 
 Vector RigidBodySystem::RBD_pos( const size_t p ) {
     Vector pos = GetLeverArm(p);
-    Vector P = pos + _COM;
+    Vector P = _angularRot * pos + _COM;
     return P;
 }
 
