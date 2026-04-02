adf74b1364a908b51015a1963a331e7e8336cc88
RLucas1102 <robenoltl@gmail.com>
2026-04-01 21:56:14 -0400

    Implemented the ComputeRBDData function; RBD_Pos is implemented, but I am currently leaving out the rotation matrix to test linear movement

--- base/RigidBodySystem.C
+++ base/RigidBodySystem.C
@@ -1,4 +1,6 @@
 #include "RigidBodySystem.h"
+#include "Vector.h"
+#include "Matrix.h"
 
 using namespace pba;
 
@@ -8,6 +10,27 @@ RigidBodySystem::RigidBodySystem(const std::string& nam) :
 
 void RigidBodySystem::ComputeRBDData() {
 
+    _COM = Vector(0,0,0);
+    _totalMass = 0.0;
+
+    for (size_t p = 0; p < Psize(); p++)
+    {
+        Vector pos = GetPos(p);
+        double mass = GetMass(p);
+
+        _totalMass += mass;
+        _COM += pos*mass;
+    }
+
+    _COM /= _totalMass;
+
+    for (size_t p = 0; p < Psize(); p++)
+    {
+        Vector pos = GetPos(p);
+        Vector d = pos - _COM;
+        SetLeverArm(p, d);
+    }
+    
 }
 
 void RigidBodySystem::ComputeM() {
@@ -16,6 +39,12 @@ void RigidBodySystem::ComputeM() {
 
 void RigidBodySystem::RecomputeMOI() {
 
+}
+
+Vector RigidBodySystem::RBD_pos( const size_t p ) {
+    Vector pos = GetLeverArm(p);
+    Vector P = pos + _COM;
+    return P;
 }
 
 RigidBody CreateRigidBody(const std::string& nam) {
