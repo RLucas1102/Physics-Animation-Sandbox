5b1a67e9f25ea64fc1e846789425317723b63b56
RLucas1102 <robenoltl@gmail.com>
2026-04-01 20:06:34 -0400

    Created rigid body class and added all variables and function declarations needed to start implementation

---
+++ include/RigidBodySystem.h
@@ -0,0 +1,60 @@
+//---------------------------------------------------------
+//
+//  Created by: Lucas Robenolt
+//
+//  Purpose: A class to hold particles for a rigid body
+//           system. There a specific functions
+//           that need to be added to a normal particle
+//           system. This class inherits from ParticleSystem
+//
+//
+//---------------------------------------------------------
+#ifndef RIGID_BODY_SYSTEM_H
+#define RIGID_BODY_SYSTEM_H
+
+#include "ParticleSystem.h"
+#include "Matrix.h"
+#include <vector>
+#include <memory>
+
+namespace pba {
+
+    class RigidBodySystem : public ParticleSystem {
+
+        public:
+            RigidBodySystem(const std::string& nam = "RigidBodyDataNoName");
+            ~RigidBodySystem(){};
+
+            void ComputeRBDData();
+            void ComputeM();
+            void RecomputeMOI();
+
+            const Matrix& InertiaMoment() const { return _momentOfInertia; }
+            const Matrix& InverseMOI() const { return _inverseMomentOfInertia; }
+            const float TotalMass() const { return _totalMass; }
+
+            // Position of a vertex in current, rotated state
+            Vector RBD_pos( const size_t p ) const;
+
+            Vector _COM;
+            Matrix _angularRot;
+            Vector _linearVel;
+            Vector _angularVel;
+            Vector _COMAcc;
+            Vector _angularAcc;
+            Vector _angularMom;
+
+        private:
+            Matrix _momentOfInertia;
+            Matrix _inverseMomentOfInertia;
+            float _totalMass;
+
+    };
+
+    typedef std::shared_ptr<RigidBodySystem> RigidBody;
+
+    RigidBody CreateRigidBody(const std:: string& nam = "RigidBodyDataNoName");
+
+}
+
+#endif
